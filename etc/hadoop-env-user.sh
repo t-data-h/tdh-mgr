56838ff564bbc5809ec2f2b48cf78f1cb0d8ea54
tca <tcarland@gmail.com>
2019-04-22 14:10:14 -0700

    sync version

--- etc/hadoop-env-user.sh
+++ etc/hadoop-env-user.sh
@@ -5,7 +5,7 @@
 #  Timothy C. Arland <tcarland@gmail.com>
 
 export HADOOP_ENV_USER=1
-export HADOOP_ENV_USER_VERSION="0.520"
+export HADOOP_ENV_USER_VERSION="0.521"
 
 
 # Assume that JAVA_HOME is already set or managed by the system.
