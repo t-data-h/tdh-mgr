c6a7c401f08d2fd4f8f2c7d874e914dbe1c22f38
tca <tcarland@gmail.com>
2019-05-30 06:45:01 -0700

    add provision script for configuring spark external shuffle for YARN

---
+++ bin/05-spark-provision.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+#
+#  Ensure Spark's External Shuffle is configured properly
+#
+PNAME=${0##*\/}
+AUTHOR="Timothy C. Arland <tcarland@gmail.com>"
+
+# ----------- preamble
+HADOOP_ENV="tdh-env-user.sh"
+
+if [ -r "./etc/$HADOOP_ENV" ]; then
+    . ./etc/$HADOOP_ENV
+elif [ -r "/etc/hadoop/$HADOOP_ENV" ]; then
+    . /etc/hadoop/$HADOOP_ENV
+elif [ -r "/opt/TDH/etc/$HADOOP_ENV" ]; then
+    . /opt/TDH/etc/$HADOOP_ENV
+elif [ -r "$HOME/hadoop/etc/$HADOOP_ENV" ]; then
+    . $HOME/hadoop/etc/$HADOOP_ENV
+fi
+
+if [ -z "$TDH_VERSION" ]; then
+    echo "Fatal! Unable to locate TDH Environment '$HADOOP_ENV'"
+    exit 1
+fi
+# -----------
+
+SPARK_PATH=$(readlink -f $SPARK_HOME)
+
+if ! [ -d $SPARK_PATH ]; then
+    echo "Error determining real path to SPARK_HOME: $SPARK_HOME"
+    exit 1
+fi
+
+SPARK_JAR=$(ls -1 $SPARK_PATH/yarn/*shuffle*.jar)
+YARN_LINK=$(ls -1 $HADOOP_HOME/share/hadoop/yarn/lib/*shuffle*.jar)
+YARN_JAR=$(readlink -f $YARN_LINK)
+
+if [ -z "$SPARK_JAR" ]; then
+    echo "Fatal Error locating the Spark Shuffle JAR"
+    exit 1
+fi
+
+if [ "$YARN_JAR" == "$SPARK_JAR" ]; then
+    echo "Spark External Shuffle Jar for YARN is already linked to: "
+    echo "  $SPARK_JAR"
+else
+    if [ -n "$YARN_LINK" ]; then
+        ( rm $YARN_LINK )
+    fi
+    ( ln -s $SPARK_JAR $HADOOP_HOME/share/hadoop/yarn/lib 
+fi
+
+echo "$PNAME Finished."
+exit 0
