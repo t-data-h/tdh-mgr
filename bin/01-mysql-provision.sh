09985302d28e1398786e7ec223175a4ae9f48961
tca <tcarland@gmail.com>
2019-07-12 12:27:15 -0700

    sync config updates

--- bin/01-mysql-provision.sh
+++ bin/01-mysql-provision.sh
@@ -16,9 +16,6 @@ if [ $rt -ne 0 ]; then
     exit $rt
 fi
 
-mysql -e "GRANT ALL PRIVILEGES ON metastore.* TO 'hive'@'localhost' IDENTIFIED BY 'TDH@hive11b'"
-mysql -e "GRANT ALL PRIVILEGES ON metastore.* TO 'hive'@'${HOST}' IDENTIFIED BY 'TDH@hive11b'"
-
 # import schema (note that this may require an update to hive-txn schema path
 mysql metastore < /opt/TDH/hive/scripts/metastore/upgrade/mysql/hive-schema-1.2.0.mysql.sql
 
@@ -28,13 +25,4 @@ if [ $rt -ne 0 ]; then
     exit $rt
 fi
 
-# hue
-mysql -e 'CREATE DATABASE IF NOT EXISTS hue'
-mysql -e "GRANT ALL PRIVILEGES ON hue.* TO 'hue'@'localhost' IDENTIFIED BY 'TDH@huexp11b'"
-mysql -e "GRANT ALL PRIVILEGES ON hue.* TO 'hue'@'${HOST}' IDENTIFIED BY 'TDH@huexp11b'"
-
-rt=$?
-
 exit $rt
-
-
