2706da54f886ba0aa7174f694a0a1b406cf990ae
tca <tcarland@gmail.com>
2019-07-18 15:02:37 -0700

    use hadoop_home var in classpath settings

--- etc/tdh-env-user.sh
+++ etc/tdh-env-user.sh
@@ -5,7 +5,7 @@
 #  Timothy C. Arland <tcarland@gmail.com>
 
 export TDH_ENV_USER=1
-export TDH_VERSION="0.7.9"
+export TDH_VERSION="0.8.0"
 
 
 # Assume that JAVA_HOME is already set or managed by the system.
