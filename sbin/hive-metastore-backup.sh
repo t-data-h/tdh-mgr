666c97ee9248c1f6bc20f91c7b31b33229700c69
tca <tcarland@gmail.com>
2019-07-28 13:41:30 -0700

    update metastore backup script

--- sbin/hive-metastore-backup.sh
+++ sbin/hive-metastore-backup.sh
@@ -1,12 +1,27 @@
-
+#!/bin/bash
+#
+#  Do a full Hive Metastore dump including the schema
 
 
 dbhost="$1"
 dbport="$2"
 dbname="$3"
 
-( mysqldump -u hive -p -h $dbhost -P $dbport --opt $dbname > $dbname-$host-backup.sql )
+if [ -z "$dbhost" ]; then
+    echo "Usage: $0  [dbhost] <dbport> <dbname>"
+    echo "  port will default to 3306 and dbname to 'metastore'"
+    exit 1
+fi
 
-(  mysqldump -u hive -p -h $dbhost -P $dbport --skip-add-drop-table --no-data $dbname > hive-$dbhost-schema-1.2.1.mysql.sql )
+if [ -z "$dbport" ]; then
+    dbport="3306"
+fi
+if [ -z "$dbname" ]; then 
+    dbname="metastore"
+fi
 
+( mysqldump -u hive -p -h $dbhost -P $dbport --opt $dbname > $dbname-$dbhost-backup.sql )
 
+( mysqldump -u hive -p -h $dbhost -P $dbport --skip-add-drop-table --no-data $dbname > hive-$dbhost-schema-1.2.1.mysql.sql )
+
+exit $?
