9b795175c9f7db9f0f7f61a95d51db36e9787a92
Timothy C. Arland <tcarland@users.noreply.github.com>
2019-08-07 16:09:31 -0700

    V0.8.7 (#14)
    * move one-time provisiong scripts to 'sbin'
    * add broker id to brokers list for host auto-provisioning

--- etc/tdh-env-user.sh
+++ etc/tdh-env-user.sh
@@ -5,7 +5,7 @@
 #  Timothy C. Arland <tcarland@gmail.com>
 export TDH_ENV_USER=1
 
-export TDH_VERSION="0.8.6"
+export TDH_VERSION="0.8.7"
 
 # JAVA_HOME should already be set or managed by the system.
 if [ -z "$JAVA_HOME" ]; then
