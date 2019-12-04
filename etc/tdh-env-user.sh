0771f32f4cd444bcf77414b22e086ace02b438d4
Timothy C. Arland <tcarland@users.noreply.github.com>
2019-12-04 12:26:10 -0800

    V0.9.5 (#19)
    * add easyrsa support scripts

--- etc/tdh-env-user.sh
+++ etc/tdh-env-user.sh
@@ -6,7 +6,7 @@
 AUTHOR="Timothy C. Arland <tcarland@gmail.com>"
 
 export TDH_ENV_USER=1
-export TDH_VERSION="0.9.4"
+export TDH_VERSION="0.9.5"
 
 # JAVA_HOME should already be set or managed by the system.
 if [ -z "$JAVA_HOME" ]; then
