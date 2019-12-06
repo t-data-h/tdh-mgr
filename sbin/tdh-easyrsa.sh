3c85fd754631ea2ad16e708c1896633186ef971d
tca <tcarland@gmail.com>
2019-12-06 09:39:45 -0800

    move req and sign to one script

---
+++ sbin/tdh-easyrsa.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+#
+#
+PNAME=${0##*\/}
+
+easyrsa=
+reqorsign=0
+rt=1
+
+usage()
+{
+    echo ""
+    echo "Usage: $PNAME [options] [action] host1 host2 ..."
+    echo "  -e|--easyrsa <path>  : Path to easyrsa pki"
+    echo "  Action is either 'gen' or 'sign'"
+    echo "  TDH_HOSTS can be set to provide list of hosts and "
+    echo "  will override any provided hosts"
+    echo ""
+}
+
+
+while [ $# -gt 0 ]; do
+    case "$1" in
+        -h|--help)
+            usage
+            exit 0
+            ;;
+        -e|--easyrsa)
+            easyrsa="$2"
+            shift
+            ;;
+        *)
+            action="$1"
+            shift
+            hosts="$@"
+            shift $#
+            ;;
+    esac
+    shift
+done
+
+if [ -n "$TDH_HOSTS" ]; then
+    hosts="$TDH_HOSTS"
+fi
+
+if [ -z "$hosts" ]; then
+    echo "Error, No hosts provided!"
+    usage
+    exit $rt
+fi
+
+if [ -n "$easyrsa" ] && [ -d $easyrsa ]; then
+    cd $easyrsa
+fi
+
+if ! [ -e "./easyrsa" ]; then
+    echo "Error locating EasyRSA!"
+    echo " Please provide the path the easyrsa3 pki directory via -e|--easyrsa"
+    exit $rt
+fi
+
+echo ""
+if [ $action == "req" ]; then
+    echo "$PNAME Generating Certificate requests for: $hosts"
+elif [ $action == "sign" ]; then
+    echo "$PNAME Signing certificate requests for: $hosts"
+    reqorsign=1
+else
+    echo "Invalid action. Valid option should be 'req' or 'sign'"
+    exit $rt
+fi
+
+
+for hostname in $hosts; do
+    shortname=${hostname%%\.*}
+
+    if [ -z "$shortname" ]; then
+        echo "Error! Provided hostname '$hostname' is not a fully qualified domain name"
+        exit $rt
+    fi
+
+    if [ $reqorsign -eq 0 ]; then   # gen-req
+        ( ./easyrsa gen-req $hostname nopass )
+    else   # sign req
+        ( ./easyrsa --subject-alt-name="DNS:${hostname},DNS:${shorntname}" sign-req serverclient $hostname )
+    fi
+
+    rt=$?
+done
+
+echo "$PNAME Finished."
+exit $rt
