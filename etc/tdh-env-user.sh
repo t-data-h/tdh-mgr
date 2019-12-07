da4a364f2b4873f4bdc3ba3229aca971fa8f9451
tca <tcarland@gmail.com>
2019-12-06 16:28:32 -0800

    sync

--- etc/tdh-env-user.sh
+++ etc/tdh-env-user.sh
@@ -6,7 +6,7 @@
 AUTHOR="Timothy C. Arland <tcarland@gmail.com>"
 
 export TDH_ENV_USER=1
-export TDH_VERSION="0.9.6"
+export TDH_VERSION="0.9.7"
 
 # JAVA_HOME should already be set or managed by the system.
 if [ -z "$JAVA_HOME" ]; then
