3ae03df8910a5e454c2a8924f05e74b70aee3096
tca <tcarland@gmail.com>
2019-12-19 14:22:58 -0800

    sync further readme updates and remerge for zksplit branch

--- etc/tdh-env-user.sh
+++ etc/tdh-env-user.sh
@@ -6,7 +6,7 @@
 AUTHOR="Timothy C. Arland <tcarland@gmail.com>"
 
 export TDH_ENV_USER=1
-export TDH_VERSION="1.0.1"
+export TDH_VERSION="1.0.2"
 
 # JAVA_HOME should already be set or managed by the system.
 if [ -z "$JAVA_HOME" ]; then
