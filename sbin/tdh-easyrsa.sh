a115c33dd417b95a340877c32f560f49d28aa856
tca <tcarland@gmail.com>
2020-08-10 16:32:03 -0700

    add x509type parameter minor fixes and script rename

--- sbin/tdh-easyrsa.sh
+++ sbin/tdh-easyrsa.sh
@@ -1,31 +1,40 @@
 #!/bin/bash
 #
-# Wrapper script for using EasyRSA3 to generate and optionally
-# sign host certificates.
+# Wrapper script for using EasyRSA3 to generate and sign host certificates.
 # This utilizes an existing EasyRSA installation, which should be
 # provided by the -e or --easyrsa parameter.
+# This also expects an x509type of 'serverclient' which may not be defined
+# in the original EasyRSA3 repository.
+# Simply copy the 'server' profile and add 'clientAuth' to the extended options.
+#  extendedKeyUsage = serverAuth,clientAuth
 #
 PNAME=${0##*\/}
+VERSION="V0.5.1"
 
-easyrsa=
+easyrsa="./easyrsa3"
 reqorsign=
-rt=1
+x509type="serverclient"  # options are 'server', 'client', or 'serverclient'
+
+# ------------------------
 
 usage()
 {
     echo ""
     echo "Usage: $PNAME [options] [action] host1 host2 ..."
+    echo "  -e|--easyrsa <path>  : Path to EasyRSA3 distribution"
     echo "  -h|--help            : Display help info and exit"
-    echo "  -e|--easyrsa <path>  : Path to EasyRSA (with existing pki)."
-    echo "    <action>           : Action is either 'gen-req' or 'sign'."
+    echo "  -x|--x509type <type> : Name of x509 type (default is 'serverclient')"
+    echo "  -V|--version         : Show version info and exit"
     echo ""
     echo "  TDH_HOSTS can be set to provide list of hosts and "
-    echo "  will override any provided hosts"
+    echo "  will override any command provided hosts"
     echo ""
 }
 
-#
+# ------------------------
 # MAIN
+#
+rt=1
 
 while [ $# -gt 0 ]; do
     case "$1" in
@@ -37,6 +46,14 @@ while [ $# -gt 0 ]; do
             easyrsa="$2"
             shift
             ;;
+        -x|--x509type)
+            x509type="$2"
+            shift
+            ;;
+        -V|--version)
+            echo "$PNAME $VERSION"
+            exit 0
+            ;;
         *)
             action="$1"
             shift
@@ -46,7 +63,6 @@ while [ $# -gt 0 ]; do
     esac
     shift
 done
-
 
 if [ -n "$TDH_HOSTS" ]; then
     hosts="$TDH_HOSTS"
@@ -62,19 +78,18 @@ if [ -n "$easyrsa" ] && [ -d "$easyrsa" ]; then
     cd $easyrsa
 fi
 
-if ! [ -e "./easyrsa" ]; then
-    echo "Error locating EasyRSA!"
+if ! [ -x "./easyrsa" ]; then
+    echo "Error locating EasyRSA.."
     echo " Please provide the path the easyrsa3 directory via -e|--easyrsa"
     echo " Specifically the path to the easyrsa binary and './pki' subdir."
     exit $rt
 fi
 
-
 echo ""
 if [[ ${action,,} =~ ^gen.* ]]; then
     echo "$PNAME Generating Certificate requests.."
     reqorsign=0
-elif [[ ${action,,} =~ ^sign$ ]; then
+elif [[ ${action,,} =~ ^sign$ ]]; then
     echo "$PNAME Signing certificate requests.."
     reqorsign=1
 else
@@ -87,15 +102,18 @@ for hostname in $hosts; do
     shortname=${hostname%%\.*}
 
     if [ -z "$shortname" ]; then
-        echo "Error! Provided hostname '$hostname' is not a fully qualified domain name"
-        exit $rt
+        echo "Error, provided hostname '$hostname' is not a fully qualified domain name."
+        echo "Skipping host '$hostname'"
+        continue
     fi
 
     if [ $reqorsign -eq 0 ]; then   # gen-req
         ( cd $easyrsa; ./easyrsa gen-req $hostname nopass )
     else   # sign req
         ( cd $easyrsa; \
-         ./easyrsa --subject-alt-name="DNS:${hostname},DNS:${shortname}" sign-req serverclient $hostname )
+         ./easyrsa \
+           --subject-alt-name="DNS:${hostname},DNS:${shortname}" \
+           sign-req serverclient $hostname )
     fi
 
     rt=$?
